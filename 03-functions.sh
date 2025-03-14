500470cd9ed6de9d2aff4f34ab560d89daef2f66
Jagannnnnn <jaganmohan85220@gmail.com>
2025-03-14 12:33:29 +0530

    robooooshop1 code

---
+++ 03-functions.sh
@@ -0,0 +1,15 @@
+# if we assign a name to set of commands.it is called as function
+
+# Syntax:
+# function_name() {
+ # commands
+# }
+# Access the function
+# function_name
+
+# example
+example() {
+  echo Example Function
+}
+
+example
