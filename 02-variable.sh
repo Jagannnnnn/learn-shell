2be7a08e88eebb500176984bd435d0de3eede5ee
Jagannnnnn <jaganmohan85220@gmail.com>
2025-03-14 21:16:18 +0530

    robooooshop1 code

--- 02-variable.sh
+++ 02-variable.sh
@@ -14,3 +14,13 @@
  # we can use characters a-z, A-Z
  # we can use numbers and underscore
 
+#input variables
+#                  < variable Input>
+#    <Variable Input>  script  <variable Input>
+
+#            Env variables
+# left side input   script  Arguments
+#1.Environment variables
+#export var=data
+# on command line you run export x1=100 before executing this script
+echo x1 - $x1
