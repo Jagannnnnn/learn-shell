3e52dc43766042a6060050ec34791928616c2a73
Jagannnnnn <jaganmohan85220@gmail.com>
2025-03-15 11:45:11 +0530

    robooooshop1 code

--- 03-functions.sh
+++ 03-functions.sh
@@ -18,3 +18,20 @@ example
 
 #Variables in functions
  # In bash shell, if we declare a variable in main program,it is accessible inisde function and vice-versa
+
+ # Input variables
+ #         < variable Input >
+ # <variable Input>  function  <variable Input>
+ #          Variables / ENV variables
+ # LHS      function    RHS
+
+ example1() {
+     echo value of y - $y
+     }
+     y=200 example1
+
+example2() {
+   echo first argument - $1
+}
+
+example2 300
